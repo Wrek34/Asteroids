ba42430c01794f026b8b75968d09059e51d67ee7
Wrek34 <rakw34@gmail.com>
2025-10-24 10:24:31 -0400

    creation of game window and exit ability

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((1280, 720))
+    while 0 == 0:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black", rect=None, special_flags=0)
+        pygame.display.flip()
     print("Starting Asteroids!")
     print("Screen width: 1280")
     print("Screen height: 720")
