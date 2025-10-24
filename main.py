a75237c8b1a043c60da21ab3fc2bbfbc5d7aa0bd
Wrek34 <rakw34@gmail.com>
2025-10-24 10:42:56 -0400

    limiting FPS

--- main.py
+++ main.py
@@ -2,17 +2,28 @@ import pygame
 from constants import *
 
 def main():
+    #begin game
     pygame.init()
+    #initializes screen
     screen = pygame.display.set_mode((1280, 720))
+
+    #fps (frames per second)
+    clock = pygame.time.Clock()
+    dt = 0 #delta time
+
+    #game loop - runs screen
     while 0 == 0:
+        #allows user to exit game window
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        #window filled
         screen.fill("black", rect=None, special_flags=0)
         pygame.display.flip()
-    print("Starting Asteroids!")
-    print("Screen width: 1280")
-    print("Screen height: 720")
+        
+        #limit framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+    
 
 if __name__ == "__main__":
     main()
