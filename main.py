a17cb108d33a8f1a0f0d3d278fd0f6e63a9241fa
Wrek34 <rakw34@gmail.com>
2025-10-24 18:27:32 -0400

    splitting of asteroids

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def main():
         
             for shot in shots:
                 if asteroid.collide(shot):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
 
         #window filled
